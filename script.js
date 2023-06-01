ee36c100f2b928ac0e0c3f02d2abd0e3c0103ffb
MIDEZA-22 <mijail1993.mdzl@gmail.com>
2023-05-31 22:42:39 -0500

    Validación de la class=input

--- script.js
+++ script.js
@@ -1,47 +1,56 @@
 const input = document.querySelector(".input");
 const textarea = document.querySelector(".textarea");
 
-function btnEncriptar(){
+function validarTexto(input) {
+    var texto = input.value;
+    var textoSinAcentos = texto.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+    var textoValido = textoSinAcentos.toLowerCase().replace(/[^a-z\s]/g, "");
+
+    if (texto !== textoValido) {
+        input.value = textoValido;
+        alert("Ingresar solo letras minúsculas y sin acentos. Se ha corregido el texto.");
+    }
+}
+
+function btnEncriptar() {
     const textoEncriptado = encriptar(input.value);
     textarea.value = textoEncriptado
     input.value = "";
 }
 
-function encriptar(stringEncriptada){
+function encriptar(stringEncriptada) {
     let llavesEncriptacion = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
-    stringEncriptada = stringEncriptada.toLowerCase();
 
-    for(let i = 0; i < llavesEncriptacion.length; i++){
-        if(stringEncriptada.includes(llavesEncriptacion[i][0])){
-            stringEncriptada = stringEncriptada.replaceAll(llavesEncriptacion[i][0], llavesEncriptacion[i][1]) 
+    for (let i = 0; i < llavesEncriptacion.length; i++) {
+        if (stringEncriptada.includes(llavesEncriptacion[i][0])) {
+            stringEncriptada = stringEncriptada.replaceAll(llavesEncriptacion[i][0], llavesEncriptacion[i][1])
         }
     }
 
     return stringEncriptada
 }
 
-function btnDesencriptar(){
+function btnDesencriptar() {
     const textoDesencriptado = desencriptar(input.value);
     textarea.value = textoDesencriptado
     input.value = "";
 }
 
-function desencriptar(stringDesencriptada){
+function desencriptar(stringDesencriptada) {
     let llavesEncriptacion = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
-    stringDesencriptada = stringDesencriptada.toLowerCase();
 
-    for(let i = 0; i < llavesEncriptacion.length; i++){
-        if(stringDesencriptada.includes(llavesEncriptacion[i][1])){
-            stringDesencriptada = stringDesencriptada.replaceAll(llavesEncriptacion[i][1], llavesEncriptacion[i][0]) 
+    for (let i = 0; i < llavesEncriptacion.length; i++) {
+        if (stringDesencriptada.includes(llavesEncriptacion[i][1])) {
+            stringDesencriptada = stringDesencriptada.replaceAll(llavesEncriptacion[i][1], llavesEncriptacion[i][0])
         }
     }
 
     return stringDesencriptada
 }
 
-function btnCopiar(){
+function btnCopiar() {
     textarea.select();
     document.execCommand("copy");
 
     alert("El texto ha sido copiado al portapapeles: " + textarea.value);
-  }
+}
